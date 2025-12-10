79043693f8772ed9abb2f1bd12907f7fdbba3dd1
Gunatheeban Sathasivam <gunatheeban@gmail.com>
2025-12-10 22:22:06 +0530

    MajorityElement_15

--- Lecode/src/MajorityElement_15.java
+++ Lecode/src/MajorityElement_15.java
@@ -1,24 +1,37 @@
-public class ReverseString_14 {
-  public boolean reverseString(char[] s) {
-    int charLength = s.length - 1;
-    for (int i = 0; i < charLength; i++) {
-      if (i >= (charLength - i)) {
-        break;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class MajorityElement_15 {
+
+  public int majorityElement(int[] nums) {
+    Map<Integer, Integer> integerMap = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+      if ( integerMap.containsKey(nums[i])) {
+        int i1 = integerMap.get(nums[i]) + 1;
+        integerMap.put(nums[i], i1);
+      } else {
+        integerMap.put(nums[i], 0);
       }
-      char temp = s[i];
-      s[i] = s[charLength - i];
-      s[charLength - i] = temp;
-//      System.out.println("----------------- " + i + " ----------------------");
-//      System.out.println("----------------- " + i + " == " + (charLength - i) + " ----------------------");
-//      System.out.println(s);
     }
-    return true;
+
+    int result = 0;
+    int max = 0;
+    for ( Entry entry : integerMap.entrySet()) {
+      int maxV = (Integer) entry.getValue();
+      if (max <= maxV) {
+        result = (Integer) entry.getKey();
+        max = maxV;
+      }
+
+    }
+
+    return result;
   }
 
   public static void main(String[] args) {
-    var solution = new ReverseString_14();
-    char[] s = {'A',' ','m','a','n',',',' ','a',' ','p','l','a','n',',',' ','a',' ','c','a','n','a','l',':',' ','P','a','n','a','m','a'};
-    solution.reverseString(s);
-    System.out.println(s);
+    var solution = new MajorityElement_15();
+    int[] nums = {5};
+    System.out.println(solution.majorityElement(nums));
   }
 }
