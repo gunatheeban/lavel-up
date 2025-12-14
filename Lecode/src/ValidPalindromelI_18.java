7e57cd75bd926be28159266393a2610ee24cab92
Gunatheeban Sathasivam <gunatheeban@gmail.com>
2025-12-14 16:01:54 +0530

    correction

--- Lecode/src/ValidPalindromelI_18.java
+++ Lecode/src/ValidPalindromelI_18.java
@@ -4,36 +4,32 @@ import java.util.Map;
 public class ValidPalindromelI_18 {
 
   public boolean validPalindrome(String s) {
-    int start = 0;
-    int end = s.length()-1;
-    int miss = 0;
-    while (start <= end) {
-      System.out.println(start + " -- "+ end);
-      if (!Character.isAlphabetic(s.charAt(start))) {
-        start++;
+    int left = 0;
+    int right = s.length() -1;
+    while (left <= right) {
+      if (s.charAt(left) != s.charAt(right)) {
+        return isPalindrome(s, left+1, right) || isPalindrome(s, left, right-1);
       }
-
-      if (!Character.isAlphabetic(s.charAt(end))) {
-        end--;
-      }
-
-      if (s.charAt(start) != s.charAt(end)) {
-        miss++;
-      }
-
-      start++;
-      end--;
+      left++;
+      right--;
     }
-    if (s.length() <= 3) {
-      return miss == 0;
-    } else {
-      return miss <= 1;
+    return true;
+  }
+
+  private boolean isPalindrome(String s, int left, int right) {
+    while (left <= right) {
+      if (s.charAt(left) != s.charAt(right)) {
+        return false;
+      }
+      left++;
+      right--;
     }
+    return true;
   }
 
   public static void main(String[] args) {
     var solution = new ValidPalindromelI_18();
-    System.out.println(solution.validPalindrome("abc"));
+    System.out.println(solution.validPalindrome("deeee"));
   }
 
 //  correct Approch
